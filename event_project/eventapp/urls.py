92c8b0f1d8c5d4bccbad5e5ec199518375023293
Arbaz Khan <157505728+arbaz-khan05@users.noreply.github.com>
2024-03-03 23:37:11 +0530

    Delete event_project directory
    Due do Duplication I need to Delete this

--- event_project/eventapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .views import EventListAPIView
-
-urlpatterns = [
-    path('', views.add_event, name='add_event'), 
-    path('api/', EventListAPIView.as_view(), name='event-list'),
-]
